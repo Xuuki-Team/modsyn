aef728c41d9acb900194165f2ca029d42bcc289a
Joel Nash <joelnash@xuuki.xyz>
2025-07-24 14:05:26 +0100

    update feature script

--- features.sh
+++ features.sh
@@ -1,1 +1,12 @@
+# Feature: Create csd file with 1 instr
+#   A musician needs to design sound to express the
+#   music.
+# 
+# Scenario: Process patch to generate csd file
+#   Given the file "patches/mpatch3" exists
+#   When I successfully run `./bin/modsyn patches/mpatch3 sound-design/feature.csd 4`
+#   Then `sound-design/feature.csd` should exist
+
+
 ./bin/modsyn patches/mpatch3 sound-design/feature.csd 4
+cat sound-design/feature.csd
